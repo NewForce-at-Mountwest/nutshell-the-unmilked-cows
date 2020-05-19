467b02be667160916e0a81cd1cd88455db04ff07
Your Name <you@example.com>
2020-05-19 13:16:04 -0400

    added first click event

---
+++ src/news.js
@@ -0,0 +1,32 @@
+const newsApiManager = {
+
+
+
+}
+
+//event listener object
+const newsEventlisters = {
+//event listener for new article button
+newArticleButton: () =>{
+    return document.querySelector("#newArticleButton").addEventListener("click", function (){
+        console.log("click")
+        })
+}
+
+
+
+}
+
+
+
+const newsCardMaker = {
+
+
+}
+
+
+
+
+newsEventlisters.newArticleButton()
+
+
