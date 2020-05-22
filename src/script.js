59f04b18eaf2264d78f142d42afbb806246f223d
Pat Shaver <pat.shaver@icloud.com>
2020-05-22 11:12:10 -0400

    added Chat section to index.html and created new chat script files

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 import tasksAPIManagerObject from "./tasks-APIManager.js"
 import printAllTasks from "./tasks-DOMPrinter.js"
 import eventListenerObject from "./tasks-EventListeners.js"
+
+//Tasks Module Starts here
+
 //Tasks events on page load - Pat Shaver
 //Gets all existing tasks from nutshell api for logged in user, which is hardcoded as the DM username (Derek- userId:2) and Prints them to the taskContainer on page load
 tasksAPIManagerObject.getAllTasksFromAPI()
@@ -49,3 +52,4 @@ document
     });
 
 
+//Chat messages module starts here
